a31e423f6d2cde7a27ef67e3429f0aac4f689fb4
Lysandre BOURSETTE <lysandre.boursette@epitech.eu>
2024-10-17 00:34:52 +0200

    perf(regexes): Optimised UUID Regex.

--- src/main/java/fr/hashtek/tekore/common/regex/Regexes.java
+++ src/main/java/fr/hashtek/tekore/common/regex/Regexes.java
@@ -6,7 +6,7 @@ public class Regexes
 {
 
     public static final String UUID_REGEX =
-        "^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}$";
+        "^[a-fA-F0-9]{8}-([a-fA-F0-9]{4}-){3}[a-fA-F0-9]{12}$";
 
 
     /**
