8ea78d89beff9b21a94ee7a92a56c1416def3dac
Lysandre BOURSETTE <lysandre.boursette@epitech.eu>
2024-10-17 03:27:15 +0200

    now serves nothing lol

--- src/main/java/fr/hashtek/tekore/bungee/listeners/ListenerProxyDisconnect.java
+++ src/main/java/fr/hashtek/tekore/bungee/listeners/ListenerProxyDisconnect.java
@@ -1,10 +1,6 @@
 package fr.hashtek.tekore.bungee.listeners;
 
 import fr.hashtek.tekore.bungee.Tekord;
-import fr.hashtek.tekore.common.account.AccountPublisher;
-import fr.hashtek.tekore.common.constants.Constants;
-import fr.hashtek.tekore.common.player.PlayerManager;
-import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.PlayerDisconnectEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
@@ -21,20 +17,7 @@ public class ListenerProxyDisconnect
     @EventHandler
     public void onDisconnect(PlayerDisconnectEvent event)
     {
-        // TODO: Push player's account to the API.
-        CORD.getProxy().getScheduler().schedule(CORD, () -> {
-            final ProxiedPlayer player = event.getPlayer();
-
-            final PlayerManager<ProxiedPlayer> playerManager =
-                CORD.getPlayerManagersManager().getPlayerManager(player);
-
-            new AccountPublisher()
-                .sendAccountToApi(playerManager.getAccount())
-                .removeAccountFromRedis(
-                    player.getUniqueId().toString(),
-                    CORD.getRedisAccess()
-                );
-        }, Constants.FINAL_PUSH_TIME, Constants.FINAL_PUSH_TIME_UNIT);
+        // ...
     }
 
 }
