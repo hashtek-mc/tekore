bc40e399bcdb58f16c3f9fcc222f5211d429e094
Lysandre BOURSETTE <lysandre.boursette@epitech.eu>
2024-10-17 23:24:13 +0200

    feat(account>provider): Added a "hasAccount" function.

--- src/main/java/fr/hashtek/tekore/common/account/io/AccountProvider.java
+++ src/main/java/fr/hashtek/tekore/common/account/io/AccountProvider.java
@@ -1,16 +1,33 @@
-package fr.hashtek.tekore.common.account;
+package fr.hashtek.tekore.common.account.io;
 
+import fr.hashtek.tekore.common.account.Account;
 import fr.hashtek.tekore.common.constants.Constants;
 import fr.hashtek.tekore.common.data.io.AbstractProvider;
 import fr.hashtek.tekore.common.data.redis.RedisAccess;
+import org.redisson.api.options.KeysScanOptions;
 
 public class AccountProvider
     extends AbstractProvider<Account>
 {
 
+    private static final String PREFIX_KEY = Constants.ACCOUNT_PREFIX_KEY;
+
+
     public AccountProvider(RedisAccess redisAccess)
     {
-        super(redisAccess, Constants.ACCOUNT_PREFIX_KEY);
+        super(redisAccess, PREFIX_KEY);
+    }
+
+
+    /**
+     * @param   uuid    UUID to test
+     * @return  True if UUID is found in the database (if there is an account linked to that UUID)
+     */
+    public boolean hasAccount(String uuid)
+    {
+        return super.getRedissonClient().getKeys()
+            .getKeys(KeysScanOptions.defaults().pattern(PREFIX_KEY + uuid))
+            .iterator().hasNext();
     }
 
 }
