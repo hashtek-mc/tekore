d69feda88021e4d9ad156b6452bff785446362f9
Lysandre BOURSETTE <lysandre.boursette@epitech.eu>
2024-10-18 16:04:52 +0200

    feat(account>io>publisher): Updated method to push an account. See description.
    Now pushes the username in a Map (`account:name` => account's uuid),
    mainly used for faster & easier UUID querying from player names.

--- src/main/java/fr/hashtek/tekore/common/account/io/AccountPublisher.java
+++ src/main/java/fr/hashtek/tekore/common/account/io/AccountPublisher.java
@@ -4,14 +4,58 @@ import fr.hashtek.tekore.common.account.Account;
 import fr.hashtek.tekore.common.constants.Constants;
 import fr.hashtek.tekore.common.data.io.AbstractPublisher;
 import fr.hashtek.tekore.common.data.redis.RedisAccess;
+import org.redisson.api.RMap;
 
 public class AccountPublisher
     extends AbstractPublisher<Account>
 {
 
+    private static final String PREFIX_KEY = Constants.ACCOUNT_PREFIX_KEY;
+
+
     public AccountPublisher(RedisAccess redisAccess)
     {
-        super(redisAccess, Constants.ACCOUNT_PREFIX_KEY);
+        super(redisAccess, PREFIX_KEY);
+    }
+
+
+    /**
+     * @apiNote Prefer using {@link AccountPublisher#push(String, String, Account)}. Please.
+     */
+    @Override
+    public void push(String uuid, Account account)
+    {
+        super.push(uuid, account);
+    }
+
+    /**
+     * Pushes a given account to the Redis database.
+     *
+     * @param   username    Account's username
+     * @param   uuid        Account's UUID
+     * @param   account     Account to push
+     */
+    public void push(String username, String uuid, Account account)
+    {
+        super.push(uuid, account);
+        this.pushUsername(account.getUsername(), account.getUuid());
+    }
+
+    /**
+     * Stores the username in a Map in the Redis database.
+     * <p>
+     * Used for faster UUID querying from username.
+     * </p>
+     *
+     * @param   username    Username
+     * @param   uuid        UUID
+     */
+    private void pushUsername(String username, String uuid)
+    {
+        final RMap<String, String> usernameMap = super.getRedissonClient()
+            .getMap(PREFIX_KEY + username);
+
+        usernameMap.put(username, uuid);
     }
 
 }
