5e4b7068b6ff656a193096604eb6d257032e0ab8
Lysandre BOURSETTE <lysandre.boursette@epitech.eu>
2024-10-20 23:59:44 +0200

    feat(bungee>login/disconnect): Added a broadcast to player's friends (when player logs in/out)

--- src/main/java/fr/hashtek/tekore/bungee/listeners/ListenerProxyDisconnect.java
+++ src/main/java/fr/hashtek/tekore/bungee/listeners/ListenerProxyDisconnect.java
@@ -1,9 +1,18 @@
 package fr.hashtek.tekore.bungee.listeners;
 
 import fr.hashtek.tekore.bungee.Tekord;
+import fr.hashtek.tekore.common.friendship.Friendship;
+import fr.hashtek.tekore.common.friendship.FriendshipRequestState;
+import fr.hashtek.tekore.common.friendship.io.FriendshipProvider;
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.PlayerDisconnectEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
+
+import java.util.List;
+import java.util.UUID;
 
 public class ListenerProxyDisconnect
     implements Listener
@@ -11,13 +20,42 @@ public class ListenerProxyDisconnect
 
     private static final Tekord CORD = Tekord.getInstance();
 
+
+    /**
+     * Broacasts the login to player's friends.
+     *
+     * @param   player  Player
+     */
+    private void broadcastToFriends(ProxiedPlayer player)
+    {
+        final List<Friendship> friendships = new FriendshipProvider(CORD.getRedisAccess())
+            .getPlayerFriendships(player.getUniqueId().toString());
+
+        friendships.forEach((Friendship friendship) -> {
+            if (friendship.getState() != FriendshipRequestState.ACCEPTED) {
+                return;
+            }
+
+            final ProxiedPlayer target = CORD.getProxy()
+                .getPlayer(UUID.fromString(friendship.getFriendUuid(player.getUniqueId().toString())));
+
+            if (target == null) {
+                return;
+            }
+
+            target.sendMessage(new TextComponent(ChatColor.AQUA + player.getName() + ChatColor.RESET + " a quitté le serveur.. . :( :pensive:"));
+        });
+    }
+
     /**
      * Called when a player disconnects from the proxy.
      */
     @EventHandler
     public void onDisconnect(PlayerDisconnectEvent event)
     {
-        // ...
+        final ProxiedPlayer player = event.getPlayer();
+
+        this.broadcastToFriends(player);
     }
 
 }
--- src/main/java/fr/hashtek/tekore/bungee/listeners/ListenerProxyLogin.java
+++ src/main/java/fr/hashtek/tekore/bungee/listeners/ListenerProxyLogin.java
@@ -1,9 +1,18 @@
 package fr.hashtek.tekore.bungee.listeners;
 
 import fr.hashtek.tekore.bungee.Tekord;
+import fr.hashtek.tekore.common.friendship.Friendship;
+import fr.hashtek.tekore.common.friendship.FriendshipRequestState;
+import fr.hashtek.tekore.common.friendship.io.FriendshipProvider;
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.PostLoginEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
+
+import java.util.List;
+import java.util.UUID;
 
 public class ListenerProxyLogin
     implements Listener
@@ -13,12 +22,40 @@ public class ListenerProxyLogin
 
 
     /**
+     * Broacasts the login to player's friends.
+     *
+     * @param   player  Player
+     */
+    private void broadcastToFriends(ProxiedPlayer player)
+    {
+        final List<Friendship> friendships = new FriendshipProvider(CORD.getRedisAccess())
+            .getPlayerFriendships(player.getUniqueId().toString());
+
+        friendships.forEach((Friendship friendship) -> {
+            if (friendship.getState() != FriendshipRequestState.ACCEPTED) {
+                return;
+            }
+
+            final ProxiedPlayer target = CORD.getProxy()
+                .getPlayer(UUID.fromString(friendship.getFriendUuid(player.getUniqueId().toString())));
+
+            if (target == null) {
+                return;
+            }
+
+            target.sendMessage(new TextComponent(ChatColor.AQUA + player.getName() + ChatColor.RESET + " a rejoint le serveur! ! !1"));
+        });
+    }
+
+    /**
      * Called when a player joins the proxy.
      */
     @EventHandler
     public void onLogin(PostLoginEvent event)
     {
-        // ...
+        final ProxiedPlayer player = event.getPlayer();
+
+        this.broadcastToFriends(player);
     }
 
 }
