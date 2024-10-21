f37e94f2ce19eb28b1c7087ffd6bffe6434829de
Lysandre BOURSETTE <lysandre.boursette@epitech.eu>
2024-10-21 12:38:15 +0200

    feat(common>constants): Added the game prefix key.

--- src/main/java/fr/hashtek/tekore/common/constants/Constants.java
+++ src/main/java/fr/hashtek/tekore/common/constants/Constants.java
@@ -7,15 +7,16 @@ public class Constants
 
     public static final Timestamp DEFAULT_TIMESTAMP = new Timestamp(0);
 
-    /* Redis prefix keys ------------------------------------------------ */
+    /* Redis prefix keys ------------------------------------------------- */
     public static final String ACCOUNT_PREFIX_KEY = "account:";
     public static final String RANK_PREFIX_KEY = "rank:";
     public static final String GUILD_PREFIX_KEY = "guild:";
     public static final String PARTY_PREFIX_KEY = "party:";
+    public static final String GAME_PREFIX_KEY = "game:";
 
     public static final String FRIENDSHIP_PREFIX_KEY = "friendship:";
     public static final String FRIENDSHIP_INVOLVES_PREFIX_KEY = "involves:";
-    /* ------------------------------------------------------------------ */
+    /* ------------------------------------------------------------------- */
 
     public static final String DEFAULT_RANK_UUID = "4a20727b-5309-56cf-8145-1e1c24fd2cc5"; // "7cdb430d-491f-5c0e-a0f8-2be1cfc7360e";
     /*                                              ^ Admin rank                               ^ Player rank                       */
