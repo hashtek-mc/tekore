946ea0700bf9ba762f503091f14f05ef28639f9f
Lysandre BOURSETTE <lysandre.boursette@epitech.eu>
2024-11-06 01:03:21 +0100

    feat(spigot>command>party>subcommand>promote): Finished kick subcommand.

--- src/main/java/fr/hashtek/tekore/spigot/command/party/subcommand/management/SubcommandPartyKick.java
+++ src/main/java/fr/hashtek/tekore/spigot/command/party/subcommand/management/SubcommandPartyKick.java
@@ -2,8 +2,14 @@ package fr.hashtek.tekore.spigot.command.party.subcommand.management;
 
 import fr.hashtek.tekore.common.command.AbstractCommand;
 import fr.hashtek.tekore.common.command.subcommand.AbstractSubcommand;
+import fr.hashtek.tekore.common.exception.EntryNotFoundException;
 import fr.hashtek.tekore.common.exception.InvalidCommandContextException;
+import fr.hashtek.tekore.common.exception.PlayerNotInPartyException;
+import fr.hashtek.tekore.common.party.Party;
+import fr.hashtek.tekore.common.regex.Regexes;
 import fr.hashtek.tekore.spigot.Tekore;
+import net.kyori.adventure.text.Component;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
@@ -27,7 +33,51 @@ public class SubcommandPartyKick
         @NotNull String[] args
     )
     {
-        // ...
+        if (args.length != 1) {
+            player.sendMessage(Component.text(ChatColor.RED + "Wrong syntax."));
+            return;
+        }
+
+        final String targetName = args[0];
+
+        if (!Regexes.matches(Regexes.USERNAME_REGEX, targetName)) {
+            player.sendMessage(Component.text(ChatColor.RED + "Invalid username."));
+            return;
+        }
+
+        if (targetName.equalsIgnoreCase(player.getName())) {
+            player.sendMessage(Component.text(ChatColor.RED + "You can't kick yourself. If you want to leave the party, either disband it or give the ownership to another player and then leave."));
+            return;
+        }
+
+        final Party playerParty = CORE.getPlayerManagersManager()
+            .getPlayerManager(player)
+            .getAccount()
+            .getPartyManager()
+            .getCurrentParty();
+
+        if (playerParty == null) {
+            player.sendMessage(Component.text(ChatColor.RED + "You are not in a party."));
+            return;
+        }
+
+        if (!playerParty.getOwnerUuid().equals(player.getUniqueId().toString())) {
+            player.sendMessage(Component.text(ChatColor.RED + "You are not the owner of this party. Type /party leave to leave."));
+            return;
+        }
+
+        try {
+            final String kickedMemberName = playerParty.kickMember(targetName);
+            playerParty.refreshMembersRamStoredParty(player);
+
+            player.sendMessage(Component.text(ChatColor.GREEN + "You successfully kicked " + ChatColor.AQUA + kickedMemberName + ChatColor.GREEN + " out of your party!"));
+        }
+        catch (EntryNotFoundException exception) {
+            player.sendMessage(Component.text(ChatColor.RED + "This player doesn't exist or never connected to this server."));
+        }
+        catch (PlayerNotInPartyException exception) {
+            player.sendMessage(Component.text(ChatColor.RED + "This player is not in your party."));
+        }
     }
 
 }
