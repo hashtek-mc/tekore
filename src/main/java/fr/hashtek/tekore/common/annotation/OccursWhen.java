812b421f1a00b665f2e29cb700ccc26900755b4e
Lysandre BOURSETTE <lysandre.boursette@epitech.eu>
2024-11-06 01:01:04 +0100

    feat(annotation): Added OccursWhen annotation.

---
+++ src/main/java/fr/hashtek/tekore/common/annotation/OccursWhen.java
@@ -0,0 +1,23 @@
+package fr.hashtek.tekore.common.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * <p>
+ * Indicates the condition(s) under which the annotated code is expected to execute.
+ * </p>
+ * <p>
+ * This annotation is used for documentation purposes and can specify the scenarios
+ * when certain behavior may occur.
+ * </p>
+ * <p>
+ * It is also used for cases where we except an exception but can ignore it for
+ * some reasons, because the exception can be thrown under some circumstances.
+ * </p>
+ * <p>
+ * It must be used in a comment : {@code // @OccursWhen    Some conditions...}
+ * </p>
+ */
+@Documented
+@Retention(RetentionPolicy.SOURCE)
+public @interface OccursWhen {}
