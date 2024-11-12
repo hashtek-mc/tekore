355f3e71d1a341536c54bbcdfd516d2829c2414f
Lysandre BOURSETTE <lysandre.boursette@epitech.eu>
2024-11-12 23:33:08 +0100

    feat(spigot>commands>party>subcommand>autopromote): Finished autopromote subcommand.

---
+++ src/main/java/fr/hashtek/tekore/spigot/command/party/subcommand/management/SubcommandPartyAutoPromote.java
@@ -0,0 +1,29 @@
+package fr.hashtek.tekore.spigot.command.party.subcommand.management;
+
+import fr.hashtek.tekore.common.command.subcommand.AbstractSubcommand;
+import fr.hashtek.tekore.common.exception.InvalidCommandContextException;
+import fr.hashtek.tekore.spigot.command.party.CommandParty;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class SubcommandPartyAutoPromote
+    extends AbstractSubcommand
+{
+
+    public SubcommandPartyAutoPromote(CommandParty parent)
+        throws InvalidCommandContextException
+    {
+        super(parent, "autopromote", "");
+    }
+
+
+    @Override
+    public void execute(
+        @NotNull Player player,
+        @NotNull String[] args
+    )
+    {
+        player.performCommand("party promote --auto");
+    }
+
+}
