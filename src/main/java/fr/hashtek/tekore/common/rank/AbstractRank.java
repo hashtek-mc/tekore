a365ff845bae8a3c8a0c6d58c1eb5f67748e098a
Lysandre BOURSETTE <lysandre.boursette@epitech.eu>
2024-11-15 00:53:40 +0100

    feat(rank): Made an abstract rank for Rank and (in the future) GuildRank.

---
+++ src/main/java/fr/hashtek/tekore/common/rank/AbstractRank.java
@@ -0,0 +1,116 @@
+package fr.hashtek.tekore.common.rank;
+
+import java.util.Set;
+
+public abstract class AbstractRank
+{
+
+    private final String uuid;
+    private final String name;
+    private final Set<String> permissions;
+
+
+    /**
+     * Creates a new empty rank.
+     * @apiNote Solely used for Redis stuff. Not for public use!
+     */
+    public AbstractRank()
+    {
+        this("", "", null);
+    }
+
+    /**
+     * Creates an empty Rank, just with the UUID.
+     * <br>
+     * Basically only used at the very first state of
+     * an {@link fr.hashtek.tekore.common.account.Account}, during its creation.
+     * @param   uuid    Rank's UUID
+     * @apiNote Solely used for Redis stuff. Not for public use!
+     */
+    public AbstractRank(String uuid)
+    {
+        this(uuid, "", null);
+    }
+
+    /**
+     * Creates a new Abstract Rank.
+     *
+     * @param   uuid        Rank's UUID
+     * @param   name        Rank's name
+     * @param   permissions Rank's permissions
+     */
+    public AbstractRank(
+        String uuid,
+        String name,
+        Set<String> permissions
+    )
+    {
+        this.uuid = uuid;
+        this.name = name;
+        this.permissions = permissions;
+    }
+
+
+    /**
+     * @return  Rank's UUID
+     */
+    public String getUuid()
+    {
+        return this.uuid;
+    }
+
+    /**
+     * @return  Rank's name
+     */
+    public String getName()
+    {
+        return this.name;
+    }
+
+    /**
+     * @return  Rank's permissions
+     */
+    public Set<String> getPermissions()
+    {
+        return this.permissions;
+    }
+
+    /**
+     * @param   permission  Permission to check
+     * @return  True if rank has the given permission
+     */
+    public boolean hasPermission(String permission)
+    {
+        if (this.permissions.isEmpty()) {
+            return false;
+        }
+
+        if (permission == null ||
+            permission.isEmpty() ||
+            this.permissions.contains("*") ||
+            this.permissions.contains(permission)
+        ) {
+            return true;
+        }
+
+        /* Globbing system (wildcard *) ------------------------------------ */
+        int dotIndex = -1;
+
+        do {
+            dotIndex = permission.indexOf('.', dotIndex + 1);
+            if (dotIndex == -1) {
+                break;
+            }
+
+            final String prefix = permission.substring(0, dotIndex) + ".*";
+
+            if (this.permissions.contains(prefix)) {
+                return true;
+            }
+        } while (dotIndex != -1);
+        /* ----------------------------------------------------------------- */
+
+        return false;
+    }
+
+}
--- src/main/java/fr/hashtek/tekore/common/rank/Rank.java
+++ src/main/java/fr/hashtek/tekore/common/rank/Rank.java
@@ -1,49 +1,40 @@
 package fr.hashtek.tekore.common.rank;
 
-import fr.hashtek.tekore.common.account.Account;
-
-import java.util.List;
+import java.util.Set;
 
 public class Rank
+    extends AbstractRank
 {
 
-    private final String uuid;
-    private final String name;
     private final String fullName;
     private final String shortName;
     private final String usernameColor;
-    private final List<String> permissions;
 
 
     /**
-     * Creates an empty rank.
-     *
-     * @apiNote Solely used for Redis stuff, not for public use.
+     * @see AbstractRank#AbstractRank()
      */
     public Rank()
     {
-        this("", "", "", "", "", null);
+        this(null);
     }
 
     /**
-     * Creates an empty Rank, just with the UUID.
-     * </br>
-     * Basically only used at the very first state of
-     * an {@link Account}, during its creation.
-     *
-     * @param   uuid    Rank's UUID
-     * @apiNote Solely used for Redis access. Not for public use!
+     * @see AbstractRank#AbstractRank(String)
      */
     public Rank(String uuid)
     {
-        this(uuid, "", "", "", "", null);
+        super(uuid);
+        this.fullName = null;
+        this.shortName = null;
+        this.usernameColor = null;
     }
 
     /**
      * Creates a new Rank.
      *
      * @param   uuid            Rank's UUID
-     * @param   name            Rank's raw name (used for database for example)
+     * @param   name            Rank's raw name (used in database)
      * @param   fullName        Rank's full name (in chat)
      * @param   shortName       Rank's short name (for tablist)
      * @param   usernameColor   Username color
@@ -55,32 +46,15 @@ public class Rank
         String fullName,
         String shortName,
         String usernameColor,
-        List<String> permissions
+        Set<String> permissions
     )
     {
-        this.uuid = uuid;
-        this.name = name;
+        super(uuid, name, permissions);
         this.fullName = fullName;
         this.shortName = shortName;
         this.usernameColor = usernameColor;
-        this.permissions = permissions;
     }
 
-    /**
-     * @return  Rank's UUID
-     */
-    public String getUuid()
-    {
-        return this.uuid;
-    }
-
-    /**
-     * @return  Rank's raw name (used for database for example)
-     */
-    public String getName()
-    {
-        return this.name;
-    }
 
     /**
      * @return  Rank's full name (in chat)
@@ -104,48 +78,6 @@ public class Rank
     public String getUsernameColor()
     {
         return this.usernameColor;
-    }
-
-    /**
-     * @return  Rank's permissions
-     */
-    public List<String> getPermissions()
-    {
-        return this.permissions;
-    }
-
-    /**
-     * @param   permission  Permission to check
-     * @return  True if rank has the given permission
-     */
-    public boolean hasPermission(String permission)
-    {
-        if (permission == null ||
-            permission.isEmpty() ||
-            this.permissions.contains("*") ||
-            this.permissions.contains(permission)
-        ) {
-            return true;
-        }
-
-        /* Globbing system (wildcard *) ------------------------------------ */
-        int dotIndex = -1;
-
-        do {
-            dotIndex = permission.indexOf('.', dotIndex + 1);
-            if (dotIndex == -1) {
-                break;
-            }
-
-            final String prefix = permission.substring(0, dotIndex) + ".*";
-
-            if (this.permissions.contains(prefix)) {
-                return true;
-            }
-        } while (dotIndex != -1);
-        /* ----------------------------------------------------------------- */
-
-        return false;
     }
 
 }
